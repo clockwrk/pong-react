2f610d6ffe648ae412f53f331d98d82c91d8af76
Jose Renteria <JoseRenteria.sf@outlook.com>
2018-03-04 13:58:53 -0800

    basic game loop constructed

--- src/component/Pong.js
+++ src/component/Pong.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 
 class Pong extends Component {
   constructor() {
+    super();
     this.state = {
+      screen: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        ratio: window.devicePixelRatio || 1,
+      }
 
     }
 
   }
   conponentDidMount() {
-
+    const context = this.refs.canvas.getContext('2d');
+    this.setState({ context: context});
+    this.start();
+    requestAnimationFrame(() => {this.update()});
   }
 
   update() {
@@ -19,6 +28,11 @@ class Pong extends Component {
   render() {
     return (
       <div>
+        <span>SCORE: 100</span>
+        <canvas ref="canvas"
+          width={this.state.screen.width * this.state.screen.ratio}
+          height={this.state.screen.height * this.state.screen.ratio}
+        />
       </div>
     );
   }
